a8748cda4a9d6114852578f0c20acd8c860e9f23
Ely De La Cruz <elycruz@elycruz.com>
2015-03-03 09:28:14 -0500

    Initial setup.

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-svnlog2j
+svnlog2json
 ========
 
-A module for printing `svn log` calls to json.
+## Description
+`svnlog2json` is a decorator for the `svn log` command.  All it does
+is offer the capability to output the `svn log` result in JSON format.
+
+The module also offers the option to save your output to a file via
+the `--output-path` param option.  Use this module the same way you
+would use `svn log`.  All params passed to this module are passed to
+`svn log` in turn with the exception of the `--output-path`.
+
+### Usage
+
+`svnlog2json [PATH] [params]`
+`svnlog2json URL[@REV] [PATH...] [params]`
+
+- `[params]` are any number of svn options for `svn log` along with
+    the `--output-path` param (@see below).  Note: All `[params]` are
+    optional.
+- `--output-path` tells `svnlog2json` to output it's output and to
+    output it to the location specified by this parameter.
+
+### License
+- ISC (http://opensource.org/licenses/ISC)
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 /**
  * Created by edelacruz on 3/2/2015.
- * @requires program: svn to be on the system path
+ * @requires @program: svn to be on the system path
  */
 
 'use strict';
@@ -13,36 +13,118 @@ var fs = require('fs'),
     path = require('path'),
     exec = require('child_process').exec,
     dargs = require('dargs'),
-    argv = require('yargs').argv,
+    argv = require('yargs')
+
+        // Param aliases (needed so that they are translated to our full name params here (via `yargs`))
+        .alias('op' , 'outputPath')
+        .alias('c'  , 'change')
+        .alias('x'  , 'extensions')
+        .alias('l'  , 'limit')
+        .alias('q'  , 'quiet')
+        .alias('r'  , 'revision')
+        .alias('g'  , 'useMergeHistory')
+        .alias('v'  , 'verbose')
+        .argv,
+
+    xml2js = require('xml2js'),
     log = console.log;
 
 module.exports = (function () {
 
-// Sudo code
-    // If not a valid call (check passed in params)
+    // If not a valid call exit
+    if (argv._.length === 0) {
+
         // Print help message(s)
+        log(fs.readFileSync(path.join(__dirname, 'README.md'), {encoding: 'utf-8'}));
+
         // Exit process
+        // process.exit(0);
+        return;
+    }
 
-    // Compile svn options to a concatenated string
+    // Setup command string to execute
+    var cmdString = 'svn log ' + argv._.shift() + ' ',
 
-    // Compose command string
+        // Allowed command string args
+        allowedCmdStringArgs = [
+                'change',             // (-c) ARG
+                'depth',              // ARG
+                'diff',               //
+                'diffCmd',            // CMD
+                'extensions',         // (-x) ARG
+                'incremental',        //
+                'internalDiff',       //
+                'limit',              // (-l) NUM
+                'quiet',              // (-q)
+                'revision',           // (-r) REV
+                'stopOnCopy',         //
+                'targets',            // FILENAME
+                'useMergeHistory',    // (-g)
+                'verbose',            // (-v)
+                'withAllRevprops',    //
+                'withNoRevprops',     //
+                'withRevprop',        // ARG
+                'xml'                 //
+        ],
 
-    // Debugging
-    log(argv);
+        // Args to append to command string
+        cmdStringArgs = {
+            xml: true,
+            limit: 100
+        };
 
-    var svnlog_process = exec('svn log http://svn-root-here.com/ --limit 3', function (err, stdout, stderr) {
+    // Merge allowed command string args passed in
+    Object.keys(argv).forEach(function (key) {
+            if (allowedCmdStringArgs.indexOf(key) > -1) {
+                cmdStringArgs[key] = argv[key];
+            }
+        });
 
+    // Append path args if any
+    if (argv._.length > 0) {
+        cmdString += argv._.join(' ') + ' ';
+    }
+
+    // Append args for svn
+    cmdString += dargs(cmdStringArgs).join(' ');
+
+    // Execute command and get a handle to it
+    exec(cmdString, function (err, stdout, stderr) {
+
+        // If err
         if (!sjl.empty(err)) {
-            log(err);
+            log('An error occurred:\n', err, '\n');
         }
 
+        // If stderr
         if (!sjl.empty(stderr)) {
-            log(stderr);
+            log('A standard error occurred:\n', stderr, '\n');
         }
 
+        // If output print/log it
         if (!sjl.empty(stdout)) {
-            log(stdout);
-        }
-    });
 
-}());
+            // Print standard output
+            log('standard output:\n', stdout, '\n');
+
+            // If output to path
+            if (!sjl.empty(argv.outputPath) && sjl.classOfIs(argv.outputPath, 'String')) {
+                xml2js.parseString(stdout, function (xml2jsError, result) {
+                    if (!sjl.empty(xml2jsError)) {
+                        log('xml2js `parseString` error:\n', xml2jsError);
+                        return;
+                    }
+                    var pathToWriteTo = path.resolve(process.cwd(), argv.outputPath);
+                    log('Output written to "' + pathToWriteTo + '".\n');
+                    fs.writeFileSync(pathToWriteTo, JSON.stringify(result));
+
+                    log('svnlog2j completed successfully.');
+                });
+
+            } // /if output-path
+
+        } // /if stdout
+
+    }); // /exec
+
+}()); // /module.exports
